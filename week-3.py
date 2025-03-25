944082e7652b2387c8210319573c39b3c49c49ba
Marywangare907 <wangariann305@gmail.com>
2025-03-25 12:51:48 +0300

    week 3

---
+++ week-3.py
@@ -0,0 +1,14 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        return price * ( discount_percent / 100)
+    else:
+        return price
+
+def main():
+    price = int(input("Enter Amount: "))
+    discount_percent = int(input("Enter Discount: "))
+    final_price = calculate_discount(price, discount_percent)
+    print(f"The final price after applying the discount is: {final_price}")
+
+if __name__ == "__main__":
+    main()
